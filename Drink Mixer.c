4142ec8b4cbb016c4021ffac9140ac21966ff5c3
jpaduchowski <jpaduchowski@hotmail.com>
2017-11-20 17:34:09 -0500

    Added main file and file to put drink mixer functions in

---
+++ Drink Mixer.c
@@ -0,0 +1,1 @@
+//Merge all functions related to valve and cart control here pls
---
+++ Main.c
@@ -0,0 +1,14 @@
+#include "Coin Sorter.c"
+#include "Menu Selections.c"
+#include "Drink Mixer.c"
+
+/*
+All functions related to coin sorter will be merged to Coin Sorter.c
+All functions related the drink selection and GUI will be merged to Menu Selections.c
+All functions related to the valve and cart control will be merged to Drink Mixer.c
+*/
+
+task main()
+{
+
+}
